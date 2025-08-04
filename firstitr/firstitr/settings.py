49dd897c7ed47e950c7f7a431f2287719d5250d1
arsh0603 <ar36639@netapp.com>
2025-08-04 16:36:18 +0530

    [ removing persistent storage if any ]

--- firstitr/firstitr/settings.py
+++ firstitr/firstitr/settings.py
@@ -73,12 +73,13 @@ WSGI_APPLICATION = 'firstitr.wsgi.application'
 
 
 # Database
+# Since this project uses file-based caching only, using minimal in-memory database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': ':memory:',  # In-memory database for minimal overhead
     }
 }
 
